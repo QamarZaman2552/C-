9eddc0044462ab30ad0d5858b2a0e0db01231d92
QamarZaman2552 <qamarbaloch2023@gmail.com>
2025-10-21 21:32:33 +0500

    constructor practice

--- classesAndPrograam.cs
+++ classesAndPrograam.cs
@@ -13,23 +13,44 @@ namespace classesAndPrograam
 
         //     System.Console.WriteLine("DefultConstructor is called");
         // }
-        public string name;
+        public int salary;
         public int age; 
+
+        public int id;  
         // Parameterized Constructor
-        public Student(string name, int age)
+        // public Student(string name, int age)
+        // {
+        //     this.name = name;
+        //     this.age = age;
+        // }
+
+        //overloaded Constructor
+        public Student()
         {
-            this.name = name;
+            Console.WriteLine($"deful constructor is invoked");
+            
+        }
+        public Student(int age, int salary)
+        {
             this.age = age;
+            this.salary = salary;
+        }
+        public Student(int age, int salary,int id)
+        {
+            this.age = age;
+            this.salary = salary;  
+            this.id =id; 
         }
         public void getdata()
         {
-            Console.WriteLine("Student name is :{0}", name);
-            Console.WriteLine("Student age is :{0}",age);
+            Console.WriteLine("Student salary is :{0}", salary);
+            Console.WriteLine("Student age is :{0}", age);
+            Console.WriteLine("student bonus is:{0}",id);
         }
 
         static void Main(string[] args)
         {
-            Student std1 = new Student("Qamar", 22);
+            Student std1 = new Student();
             std1.getdata();
              Console.ReadLine();
         }
