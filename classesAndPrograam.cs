55d09e619723f5806998e7293b39ad0be901b93e
QamarZaman2552 <qamarbaloch2023@gmail.com>
2025-10-21 22:23:27 +0500

    constructor practice

--- classesAndPrograam.cs
+++ classesAndPrograam.cs
@@ -14,8 +14,8 @@ namespace classesAndPrograam
         //     System.Console.WriteLine("DefultConstructor is called");
         // }
         public int salary;
-        public int age; 
-
+        public int age;
+        public string distance;
         public int id;
         // Parameterized Constructor
         // public Student(string name, int age)
@@ -25,39 +25,66 @@ namespace classesAndPrograam
         // }
 
         //overloaded Constructor
-        public Student()
-        {
-            Console.WriteLine($"deful constructor is invoked");
 
-        }
+        // public Student()
+        // {
+        //     Console.WriteLine($"deful constructor is invoked");
+
+        // }
         // Parameterized Constructor (2 parameters)
-        public Student(int age, int salary)
+
+        // public Student(int age, int salary)
+        // {
+        //     this.age = age;
+        //     this.salary = salary;
+        // }
+        // Parameterized Constructor (3 parameters)
+
+        // public Student(int age, int salary,int id)
+        // {
+        //     this.age = age;
+        //     this.salary = salary;  
+        //     this.id =id; 
+        // }
+
+
+        // CopyConstructor Constructor
+        public Student(int age, int salary, int id)
         {
             this.age = age;
             this.salary = salary;
+            this.id = id;
         }
-         // Parameterized Constructor (3 parameters)
-        public Student(int age, int salary,int id)
+        public Student(Student S,string distance)
         {
-            this.age = age;
-            this.salary = salary;  
-            this.id =id; 
+            this.age = S.age;
+            this.salary = S.salary;
+            this.id = S.id;
+            this.distance = distance;
         }
-        public void getdata()
+         public void getdata()
         {
             Console.WriteLine("Student salary is :{0}", salary);
             Console.WriteLine("Student age is :{0}", age);
-            Console.WriteLine("student id is:{0}",id);
+            Console.WriteLine("student id is:{0}", id);
+            Console.WriteLine("Student traveled distance: {0}",distance);
+            
         }
 
 
         static void Main(string[] args)
         {
-            Student std3 = new Student();
+            // Student std3 = new Student();
             Student std1 = new Student(18, 20002, 1000);
-            Student std2 = new Student(22, 20002);
-            
+            Student std4 = new Student(std1,"500km");
+
+
+            // Student std2 = new Student(22, 20002);
+             Console.WriteLine("Original Student (std1):");
             std1.getdata();
+
+            Console.WriteLine("Copied Student (std2):");
+            std4.getdata();
             Console.ReadLine();
         }
            
--- obj/Debug/net9.0/Classes and Objects.AssemblyInfo.cs
+++ obj/Debug/net9.0/Classes and Objects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes and Objects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394eff4477a27d062453c11e88f02abb35e4e4f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee3dd180ecfcb1ffa660299fc81a2a1b215a938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes and Objects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes and Objects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
