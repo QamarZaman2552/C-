35384e15fc8fbcddbb54d7dc0254430363ab54d0
QamarZaman2552 <qamarbaloch2023@gmail.com>
2025-10-21 23:20:36 +0500

    constructor practice

--- obj/Debug/net9.0/Classes and Objects.AssemblyInfo.cs
+++ obj/Debug/net9.0/Classes and Objects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes and Objects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d09e619723f5806998e7293b39ad0be901b93e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d13e1a542db31efbfa18215536f896ea8f49fae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes and Objects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes and Objects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
